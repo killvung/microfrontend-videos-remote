acdd7147b83f0d7d193e3e450380c8a117e834bd
killvung <killvung@gmail.com>
2020-08-17 16:53:11 -0500

    Return

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,10 +5,12 @@ export default ({ video, onVideoSelect }) => {
   const url = video?.snippet.thumbnails.medium.url;
   const title = video?.snippet.title;
 
-  <div onClick={() => onVideoSelect(video)} className="video-item item">
-    <img className="ui image" src={url} alt={title} />
-    <div className="content">
-      <div className="header">{title}</div>
+  return (
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img className="ui image" src={url} alt={title} />
+      <div className="content">
+        <div className="header">{title}</div>
+      </div>
     </div>
-  </div>
+  )
 }
